dc414981b21cc131d8f8657ece1375b0b7f609a2
jungkihun <khkh0130@gmail.com>
2021-11-08 05:29:15 +0900

    jkh fome2 edit

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -50,6 +50,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "확 인";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
